6a08f63c600acdbd5a9473e5d2124e310d91b513
Anish Dangol <dangol.anish001@gmail.com>
2023-02-20 20:12:27 +0545

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
-          Smol <span className="font-bold">Eatery</span>
+          Demo <span className="font-bold">Eatery</span>
         </h1>
         <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
           <p className="bg-black  text-white rounded-full p-2">Delivery</p>
